3cf4a520087478cfc098d47c134ea33ad438b76d
Empoliam <liamjwright@btinternet.com>
2017-09-09 23:32:07 +0100

    Allows for animation generation

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,25 @@ import org.apache.commons.math3.complex.Complex;
 public class Main {
 
 	static int ITERATIONS = 256;
-	static double IMAGE_X = 3000;
-	static double IMAGE_Y = 3000;
-	static double ZOOM = 500.0;
-	static double CENTRE_X = -0.747;
-	static double CENTRE_Y = 0.1005;
-	static boolean CROSSHAIR = true;
+	static double IMAGE_X = 1280;
+	static double IMAGE_Y = 1280;
+	static double ZOOM = 1.0;
+	static double CENTRE_X = 0;
+	static double CENTRE_Y = 0;
+	static boolean CROSSHAIR = false;
+	
+	static double r = -0.734;
+	static double i = 0.144;
+	
+	static Complex c = new Complex(r,i);
 
 	static BufferedImage output = new BufferedImage((int)IMAGE_X,(int)IMAGE_Y,BufferedImage.TYPE_INT_RGB);
 
 	public static void main(String args[])
 	{
 
+		for(int l = 0; l < 500; l++) {
+		
 		long START = System.nanoTime();
 
 		for(int x = 0; x < (int)IMAGE_X; x ++){
@@ -45,20 +52,31 @@ public class Main {
 
 		}
 
-		File f = new File("mandelbrot.png");
+		File f = new File("julia" + l + ".png");
 		try { ImageIO.write(output, "PNG", f); }
 		catch(IOException e){System.out.println("Failed to print"); };
 
 		long END = System.nanoTime();
 
-		System.out.println((END - START) / 1000000000);
+		System.out.println((END - START) / 1000000 + " " + l);
 
+		r = r -0.0001;
+		r = Math.round(r*10000);
+		r = r/10000;
+		
+		i = i + 0.0001;
+		i = Math.round(i*10000);
+		i = i/10000;
+		
+		c = new Complex(r,i);
+		
+		}
+		
 	}
 
-	static int check(Complex c)
+	static int check(Complex z)
 	{
 
-		Complex z = new Complex(0.0, 0.0);
 		int escape = ITERATIONS+1;
 
 		for(int k = 1; k <= ITERATIONS; k++){
@@ -66,7 +84,7 @@ public class Main {
 			z = z.multiply(z);			
 			z = z.add(c);
 
-			if((z.getReal()*z.getReal()) + (z.getImaginary()*z.getImaginary()) > 2){
+			if(z.getReal() > 2.0 || z.getImaginary() > 2.0){
 				escape = k;
 				break;
 			}
